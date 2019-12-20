db144a6e6ec9aebdb3331cba42ca689d426004c9
David Roberts <ddr@krontech.ca>
2019-12-19 17:51:04 -0800

    Fix watch-computer using old, unset variable $CHRONOS_PASSWORD.
    	- We use key-based authentication now.

--- util/watch-computer.sh
+++ util/watch-computer.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-set -uo pipefail
+set -euo pipefail
 IFS=$'\n'
 
-trap "exit;" SIGINT SIGTERM #The following loop works fine in Konsole but won't ever stop on Gnome Terminal.
+trap "exit;" SIGINT SIGTERM
 
+cd "$(dirname "$0")/.." #Always sync project root directory.
+
+#The following loop works fine in Konsole but won't ever stop on Gnome Terminal.
 while true; do 
 	find -regex '\./[^_\.].*' ! -name git_description ! -path 'util/stats_reported' | entr -d bash -c "
 		if [[ \$(cat git_description) != \$(git describe --tags --always) ]]; then #Only update when description changes, results in some thrashing otherwise.
 			git describe --tags --always > git_description
 		fi
-		rsync -ir ./ \"${CAM_ADDRESS:-root@192.168.12.1}:/root/pychronos/\" \
-			--delete --links \
-			--rsh=\"/usr/bin/sshpass -p $CHRONOS_PASSWORD ssh -l root\" --times --inplace \
-			--exclude \"__pycache__\" --exclude \"/.git\" \
-	"
+		
+		rsync --recursive --delete --links --inplace --times --itemize-changes \
+			./ \"${CAM_ADDRESS:-root@192.168.12.1}:~/pychronos/\" \
+			--exclude \"__pycache__\" \
+			--exclude \"/.git\" \
+			--exclude \".directory\" \
+	" || true
 done
